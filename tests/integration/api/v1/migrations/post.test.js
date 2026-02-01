f0814f88591d2e33634eb7ad18c6f0af518e29b4
Vinícius Berkembrock <vinicius.marcon@clinicorp.com>
2026-02-01 16:22:02 -0300

    fix: remove unused constant from `migration\post.test`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,7 +3,7 @@ import orchestrator from "tests/orchestrator.js";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
   await orchestrator.clearDatabase();
-}, 60000);
+});
 
 describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
@@ -18,18 +18,10 @@ describe("POST /api/v1/migrations", () => {
         expect(response1.status).toBe(201);
 
         const response1Body = await response1.json();
+
         expect(Array.isArray(response1Body)).toBe(true);
-
         expect(response1Body.length).toBeGreaterThan(0);
-
-        const responseS = await fetch(
-          "http://localhost:3000/api/v1/migrations",
-          {
-            method: "POST",
-          },
-        );
       });
-
       test("For the second time", async () => {
         const response2 = await fetch(
           "http://localhost:3000/api/v1/migrations",
@@ -40,8 +32,8 @@ describe("POST /api/v1/migrations", () => {
         expect(response2.status).toBe(200);
 
         const response2Body = await response2.json();
-        expect(Array.isArray(response2Body)).toBe(true);
 
+        expect(Array.isArray(response2Body)).toBe(true);
         expect(response2Body.length).toBe(0);
       });
     });
